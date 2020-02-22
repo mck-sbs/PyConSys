24721f7e03a591975f214d84ec68d73d748fb7fc
Metin Karatas <metin.can.karatas@gmail.com>
2020-02-22 13:00:32 +0100

    Update copyright

--- pyconsys/PT2.py
+++ pyconsys/PT2.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# 2020, Metin Karatas (m.karatas@sbs-herzogenaurach.de)
+# (c) 2020, Metin Karatas (m.karatas@sbs-herzogenaurach.de)
 
 from pyconsys.Control import Control
 
--- pyconsys/__init__.py
+++ pyconsys/__init__.py
@@ -2,6 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-# 2020, Metin Karatas (m.karatas@sbs-herzogenaurach.de)
+# (c) 2020, Metin Karatas (m.karatas@sbs-herzogenaurach.de)
 
 __all__ = ["DControl", "IControl", "PControl", "PT1", "PT2", "PIDControl"]
